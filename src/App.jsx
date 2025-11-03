09fec8a1dae19ba62116069d8fd59b3779352f07
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-04 02:01:36 +0800

    Subject

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ function App() {
 
   return (
     <>
-      <Card image="https://i.pinimg.com/1200x/68/18/8d/68188d6e515e2196a2f2e506cc4e1a33.jpg" title="Magari Isaki" text="The Quick Brown Fox Jumps Over The Lazy Dog"/>
+      <div className='border-2 p-1.5 max-w-md grid grid-cols-2 '>
+        <Card image="https://i.pinimg.com/1200x/68/18/8d/68188d6e515e2196a2f2e506cc4e1a33.jpg" title="Magari Isaki" text="The Quick Brown Fox Jumps Over The Lazy Dog"/>
 
-      <Card image="https://i.pinimg.com/736x/9e/fa/79/9efa79625639b129f08012a2a585a9c2.jpg" title="Hiro Segawa" text="The Quick Brown Fox Jumps Over The Lazy Dog"/>
+        <Card image="https://i.pinimg.com/736x/9e/fa/79/9efa79625639b129f08012a2a585a9c2.jpg" title="Hiro Segawa" text="The Quick Brown Fox Jumps Over The Lazy Dog"/>
+      </div>
+
     </>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,8 +5,8 @@ function Card({image, title, text}) {
 
     return (
         <>
-            <div>
-                <img src={image} alt="" className="w-3xs rounded-xl"/>
+            <div className="p-1.5">
+                <img src={image} alt="" className="rounded-xl w-full"/>
                 <h1>{title}</h1>
                 <p>{text}</p>
             </div>
