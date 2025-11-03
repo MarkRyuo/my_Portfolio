783a00371baf9502f88af86e7ef4bb035b0f7bbb
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-03 19:45:28 +0800

    Subject

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,7 +6,7 @@ function Card({image,title, text}) {
     return (
         <>
             <div>
-                <img src={image} alt="" className="w-3xs"/>
+                <img src={image} alt="" className="w-3xs rounded-xl"/>
                 <h1>{title}</h1>
                 <p>{text}</p>
             </div>
