f803833a174bc2534f4d0b801650aecdcc4e7e9c
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-04 13:48:21 +0800

    Subject

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,19 @@
+import React, { Component } from "react"
 
 
-function Card({image, title, text}) {
-
-    return (
-        <>
+class Card extends Component {
+    render() {
+        const {image, title, text} = this.props
+        return (
+            <>
             <div className="p-1.5 border-1 max-w-xs">
                 <img src={image} alt="" className="rounded-xl w-full"/>
                 <h1 className="py-1 text-2xl">{title}</h1>
                 <p className="text-base">{text}</p>
             </div>
         </>
-    )
+        )
+    }
 }
 
 export default Card
