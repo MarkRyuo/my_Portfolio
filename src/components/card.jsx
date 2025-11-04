e78f01f4c20dbcb97bb9a9eb2e3ad753c7983372
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-04 10:34:21 +0800

    Subject

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,10 +5,10 @@ function Card({image, title, text}) {
 
     return (
         <>
-            <div className="p-1.5 border-1">
+            <div className="p-1.5 border-1 max-w-xs">
                 <img src={image} alt="" className="rounded-xl w-full"/>
-                <h1>{title}</h1>
-                <p className="text-xs">{text}</p>
+                <h1 className="py-1 text-2xl">{title}</h1>
+                <p className="text-base">{text}</p>
             </div>
         </>
     )
