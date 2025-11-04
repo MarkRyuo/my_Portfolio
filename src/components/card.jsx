f943a9145c0dc8362b98297e68cec126f57a2d25
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-04 09:23:02 +0800

    Subject

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,10 +5,10 @@ function Card({image, title, text}) {
 
     return (
         <>
-            <div className="p-1.5">
+            <div className="p-1.5 border-1">
                 <img src={image} alt="" className="rounded-xl w-full"/>
                 <h1>{title}</h1>
-                <p>{text}</p>
+                <p className="text-xs">{text}</p>
             </div>
         </>
     )
