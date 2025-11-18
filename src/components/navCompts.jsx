41127605e22c3e247c70b6a3f6774de399a042be
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-18 10:18:45 +0800

    Subject: Update

--- src/components/navCompts.jsx
+++ src/components/navCompts.jsx
@@ -1,3 +1,27 @@
+import React, { Component } from "react";
 
 
+class NavCompts extends Component {
 
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+
+        const {largeText, smallText} = this.props 
+
+        return(
+            <div>
+                <div>
+                    <h1>{largeText}</h1>
+                    <h2>{smallText}</h2>
+                </div>
+
+                <div>
+                    
+                </div>
+            </div>
+        )
+    }
+}
