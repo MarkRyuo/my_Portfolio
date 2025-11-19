c4d842fe3f03bb788acff210fbbf0772ad5b9a07
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-19 09:13:37 +0800

    Subject: Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import './App.css'
-import NavCompts from './components/navCompts'
+import "./App.css";
+import NavCompts from "./components/navCompts";
+import SocialCompts from "./components/socialCompts";
 
 function App() {
-
   return (
     <>
-      <main className='bg-[#FFFFFC]'>
-        <div className='main-container'> 
-          <div className='mx-auto w-auto max-w-7xl'>
-            <NavCompts largeText="Always Building," smallText="Always Learning"/>
-          </div>
+      <main className="bg-[#FFFFFC]">
+        <nav className="mx-auto w-auto max-w-7xl">
+          <NavCompts largeText="Always Building," smallText="Always Learning" />
+        </nav>
+        <div className="hero-container">
           <section>
-            
+            <div>
+
+            </div>
           </section>
           <section>
 
           </section>
         </div>
+        <footer className="md:hidden">
+          <SocialCompts />
+        </footer>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
