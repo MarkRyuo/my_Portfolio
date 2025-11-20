0bc044a1f87e2045dcc22f69ffda5b3ab16ba403
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-20 17:07:29 +0800

    Subject: Update

--- src/components/systemCompts.jsx
+++ src/components/systemCompts.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
 
 
 
 function SystemCompts({sysName, url}) {
 
-    const showSystem = (url) => {
-        return url
+    const {hasUrl, setUrl} = useState()
+    const {hasSysName, setSysName} = useState()
+
+    const showSystem = () => {
+        return setUrl(url)
     }
 
     return(
