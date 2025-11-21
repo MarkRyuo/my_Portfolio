9b13911e74ad808b6f67534389e671cd68f7eb42
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-21 10:11:53 +0800

    Subject: Update

--- src/components/leftHeroCompts.jsx
+++ src/components/leftHeroCompts.jsx
@@ -1,13 +1,13 @@
 function LeftHero() {
   return (
-    <div>
+    <div className="grid grid-rows-3">
       <div>
-        <p>Hello, I'm</p>
-        <p>Jhon Mark</p>
+        <p className="text-2xl">Hello, I'm</p>
+        <p className="text-5xl">Jhon Mark</p>
       </div>
 
       <div>
-        <p>
+        <p className="text-base">
           I’m a builder of end-to-end web experiences. I started with front-end
           design, but my curiosity led me deeper into servers, databases, and
           APIs. Now, I bring ideas to life from the first pixel on screen to the
@@ -15,8 +15,8 @@ function LeftHero() {
         </p>
       </div>
 
-      <div>
-        <a href="">
+      <div className="flex items-end">
+        <a href="" className="text-base border-b-2">
             Download my Resume
         </a>
       </div>
