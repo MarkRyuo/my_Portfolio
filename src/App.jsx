6bf0e2e04e2f7cf16d2e2246dd200dca01c0f51e
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-24 19:08:12 +0800

    Subject: Update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import SocialCompts from "./components/socialCompts";
 function App() {
   return (
     <>
-      <main className="bg-[#FFFFFC] relative h-screen grid grid-rows-[100px, 1fr, 100px] md:grid-rows[1fr, 2fr] ">
+      <main className="relative bg-[#FFFFFC] relative h-screen grid grid-rows-[100px, 1fr, 100px] md:grid-rows[1fr, 2fr] ">
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
         <nav className="">
           <NavCompts largeText="Always Building," smallText="Always Learning" />
         </nav>
--- src/components/socialCompts.jsx
+++ src/components/socialCompts.jsx
@@ -24,15 +24,15 @@ class SocialCompts extends Component {
         return(
             <div className="flex justify-between w-60 px-3">
                 <a href={`mailto: ${data.social.email.gmail}`} target="_black" className="max-w-7 cursor-pointer">
-                    <img src={social.gmail} alt="" srcset="" className="w-full"/>
+                    <img src={social.gmail} alt="" srcSet="" className="w-full"/>
                 </a>
 
                 <a href={data.social.linkedIn.url} target="_blank" className="max-w-7 cursor-pointer">
-                    <img src={social.linkedIn} alt="" srcset="" className="w-full"/>
+                    <img src={social.linkedIn} alt="" srcSet="" className="w-full"/>
                 </a>
 
                 <a href={data.social.gitHub.url} target="_blank" className="max-w-7 cursor-pointer">
-                    <img src={social.gitHub} alt="" srcset="" className="w-full"/>
+                    <img src={social.gitHub} alt="" srcSet="" className="w-full"/>
                 </a>
             </div>
         )
